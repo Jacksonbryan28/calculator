0886a9b9088c311597b265768aa69cbc79e47f5f
Jackson <jacksonbryan280@gmail.com>
2025-07-20 21:28:45 -0400

    created operate function to take in numbers to call a operate function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,9 @@
+let num1, num2, operator;
+
+function operate(num1, operator, num2) {
+  return add(num1, num2);
+}
+
 function add(a, b) {
   return a + b;
 }
@@ -19,3 +25,6 @@ console.log(add(23, 25));
 console.log(subtract(24, 10));
 console.log(multiply(5, 8));
 console.log(divide(100, 20));
+
+console.log("------");
+console.log(operate(3, "+", 10));
