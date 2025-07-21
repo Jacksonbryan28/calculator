f333e5a5657e9283af699819ae227350e9469754
Jackson <jacksonbryan280@gmail.com>
2025-07-20 21:08:33 -0400

    created fuctions to add, subtract, multiply, and divide

---
+++ javascript.js
@@ -0,0 +1,21 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+//test
+console.log(add(23, 25));
+console.log(subtract(24, 10));
+console.log(multiply(5, 8));
+console.log(divide(100, 20));
