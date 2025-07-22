dfe8930d3dfd54ad3a05ce4fba68edcefeb5a06d
Jackson <jacksonbryan280@gmail.com>
2025-07-21 22:55:08 -0400

    updated operate function with switch cases

--- javascript.js
+++ javascript.js
@@ -1,16 +1,6 @@
-let num1, num2, operator;
+let num1, num2, operator, currentInput;
 
 // //adds variable to button
-// const button = document.querySelector("button");
-
-// button.addEventListener("click", () => {
-//   gridDensity = prompt("Enter a number between 1-100");
-//   if (gridDensity > 0 && gridDensity <= 100) {
-//     console.log(gridDensity);
-//     resizeGrid(gridDensity);
-//   }
-// });
-
 //Selects all buttons in document
 const button = document.querySelectorAll("button");
 //links display in html
@@ -21,9 +11,9 @@ button.forEach((button) => {
   button.addEventListener("click", () => {
     console.log("button pressed");
     display.textContent += button.id;
-    num1 = display.textContent;
-    console.log(num1);
-    parseInput(num1);
+    currentInput = display.textContent;
+    console.log(currentInput);
+    parseInput(currentInput);
   });
 });
 
@@ -31,6 +21,7 @@ button.forEach((button) => {
 function parseInput(input) {
   //check if string has any operators
   switch (input.charAt(input.length - 1)) {
+    //If it has a operator in it, set that as the operator variable, and save the previous string
     case "+":
       operator = "+";
       console.log(operator);
@@ -47,34 +38,67 @@ function parseInput(input) {
       operator = "÷";
       console.log(operator);
       break;
+    //If the operator is = then execute the operate function with the variables as inputs
+    case "=":
+      operator = "=";
+      console.log(operator);
+      operate(num1, operator, num2);
+      break;
   }
 }
 
+//takes the inputs and calls the correct math function to execute based on the operator
 function operate(num1, operator, num2) {
-  return add(num1, num2);
+  let result;
+  //decide what function to call based on what operator is set to
+  switch (operator) {
+    case "+":
+      result = add(num1, num2);
+      break;
+    case "-":
+      result = subtract(num1, num2);
+      break;
+    case "⋅":
+      result = multiply(num1, num2);
+      break;
+    case "÷":
+      result = divide(num1, num2);
+      break;
+
+    // //If the operator is = then execute the operate function with the variables as inputs
+    // case "=":
+    //   operator = "=";
+    //   console.log(operator);
+    //   operate(num1, operator, num2);
+    //   break;
+  }
+  return result;
 }
 
+//operation functions
 function add(a, b) {
   return a + b;
 }
-
 function subtract(a, b) {
   return a - b;
 }
-
 function multiply(a, b) {
   return a * b;
 }
-
 function divide(a, b) {
   return a / b;
 }
 
-//test
-console.log(add(23, 25));
-console.log(subtract(24, 10));
-console.log(multiply(5, 8));
-console.log(divide(100, 20));
+//test math functions
 
-console.log("------");
-console.log(operate(3, "+", 10));
+// console.log(add(23, 25));
+// console.log(subtract(24, 10));
+// console.log(multiply(5, 8));
+// console.log(divide(100, 20));
+
+console.log("---test operate function---");
+
+console.log(operate(20, "+", 10));
+console.log(operate(20, "-", 10));
+console.log(operate(20, "⋅", 10));
+console.log(operate(20, "÷", 10));
