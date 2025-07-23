683a005619ad36a8f3a0910f34d1112992937a89
Jackson <jacksonbryan280@gmail.com>
2025-07-23 17:05:09 -0400

    added clear ability

--- javascript.js
+++ javascript.js
@@ -12,7 +12,7 @@ button.forEach((button) => {
     console.log("button pressed");
     display.textContent += button.id;
     currentInput = display.textContent;
-    console.log(currentInput);
+    // console.log(currentInput);
     parseInput(currentInput);
   });
 });
@@ -22,36 +22,53 @@ function parseInput(input) {
   //check if string has any operators
   switch (input.charAt(input.length - 1)) {
     //If it has a operator in it, set that as the operator variable, and save the previous string
+    case "c":
+      num1 = "";
+      operator = "";
+      prevOperator = "";
+      num2 = "";
+      display.textContent = "";
     case "+":
       operator = "+";
       num1 = input.slice(0, input.length - 1);
       currentInput = "";
-      display.textContent = "+";
+      // display.textContent = "+";
       break;
-    case "-":
-      operator = "-";
+    case "m":
+      operator = "m";
+      num1 = input.slice(0, input.length - 1);
+      currentInput = "";
       console.log(operator);
       break;
     case "⋅":
       operator = "⋅";
+      num1 = input.slice(0, input.length - 1);
+      currentInput = "";
       console.log(operator);
       break;
     case "÷":
       operator = "÷";
+      num1 = input.slice(0, input.length - 1);
+      currentInput = "";
       console.log(operator);
       break;
     //If the operator is = then execute the operate function with the variables as inputs
     case "=":
+      //stores the value of what function to call by saving the operator
       prevOperator = operator;
       operator = "=";
-      console.log(operator);
+      // console.log(operator);
       //checks that num 1 and operator have values. If they do, save currentInput to num2 and run operate
       if (num1 != "" && operator != "") {
-        num2 = input.slice(1, input.length - 1);
-        console.log("num2: " + num2);
-        console.log("num 1 typeof: " + typeof num1);
-        console.log("num 2 typeof: " + typeof num2);
-        console.log("operator typeof: " + typeof operator);
+        num2 = input.slice(2, input.length - 1);
+        // console.log("num2: " + num2);
+        // console.log("num 1 typeof: " + typeof num1);
+        // console.log("num 2 typeof: " + typeof num2);
+        // console.log("operator typeof: " + typeof operator);
+
+        console.log("num 1 : " + num1);
+        console.log("num 2 : " + num2);
+        console.log("operator : " + operator);
         display.textContent = operate(num1, prevOperator, num2);
       }
       break;
@@ -61,16 +78,13 @@ function parseInput(input) {
 //takes the inputs and calls the correct math function to execute based on the operator
 function operate(a, operator, b) {
   let result;
-  console.log("operate function triggered");
   console.log(operator);
   //decide what function to call based on what operator is set to
   switch (operator) {
     case "+":
-      console.log("a = " + a);
-      console.log("b = " + b);
       result = add(a, b);
       break;
-    case "-":
+    case "m":
       result = subtract(a, b);
       break;
     case "⋅":
@@ -89,13 +103,15 @@ function add(a, b) {
   return parseInt(a) + parseInt(b);
 }
 function subtract(a, b) {
-  return a - b;
+  return parseInt(a) - parseInt(b);
 }
 function multiply(a, b) {
-  return a * b;
+  console.log(a);
+  console.log(b);
+  return parseInt(a) * parseInt(b);
 }
 function divide(a, b) {
-  return a / b;
+  return parseInt(a) / parseInt(b);
 }
 
 //test math functions
@@ -105,9 +121,9 @@ function divide(a, b) {
 // console.log(multiply(5, 8));
 // console.log(divide(100, 20));
 
-console.log("---test operate function---");
+// console.log("---test operate function---");
 
-console.log(operate(20, "+", 10));
-console.log(operate(20, "-", 10));
-console.log(operate(20, "⋅", 10));
-console.log(operate(20, "÷", 10));
+// console.log(operate(20, "+", 10));
+// console.log(operate(20, "-", 10));
+// console.log(operate(20, "⋅", 10));
+// console.log(operate(20, "÷", 10));
